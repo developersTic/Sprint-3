099d4d14937a4cb244e298b473934c9b822dce93
Faiver <fafevalo99@gmail.com>
2022-11-13 18:26:27 -0500

    configuracion de nodemailer

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -1,6 +1,17 @@
 import express from "express";
-import { prueba } from "../controllers/usuarioController.js";
+import {
+  prueba,
+  registrar,
+  confirmar,
+} from "../controllers/usuarioController.js";
+ 
 const router = express.Router();
+
 // Rutas Publicas
 router.get("/prueba", prueba);
+
+// rutas para la gestion de usurarias 
+router.post("/", registrar);
+router.get("/confirmar/:token", confirmar);
+
 export default router;
