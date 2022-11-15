6a6e21f5013c702e2cc27ee99db2e961a040dc87
Faiver <fafevalo99@gmail.com>
2022-11-14 20:20:42 -0500

    configuracion stock

--- backend/controllers/ventaController.js
+++ backend/controllers/ventaController.js
@@ -49,6 +49,101 @@ const updateVentas = async (req, res) =>{
   }
 };
 
+const getProductoStock = (producto, cantidad) => new Promise( (resolve, reject) => {
+
+  if (producto) {
+      // console.log("****************");
+      // console.log("Nombre: " , producto.nombre);
+      // console.log("Stoct: " , producto.stock);
+      // console.log("Cantidad solicitada: " , cantidad);
+      // console.log("Saldo: ", producto.stock - cantidad);
+      if(producto.stock === 0){
+          //console.log(`No hay stock del Producto ${producto.nombre}`);
+          resolve({
+              msg: `No hay stock del Producto ${producto.nombre}`,
+              precio: producto.precio,
+              cantidad,
+              subtotal: producto.precio * 0, 
+              cantidadStock: 0,
+              cantidadVenta: 0,
+              estado: false
+          });
+      }else{
+          if((producto.stock - cantidad) < 0){
+              //console.log(`La cantidad solicitada no la hay, solo le vendemos ${producto.stock} `);
+              resolve({
+                  msg: `La cantidad solicitada no la hay, solo le vendemos ${producto.stock} `,
+                  precio: producto.precio,
+                  cantidad,
+                  subtotal: producto.precio * (producto.stock), 
+                  cantidadStock: 0,
+                  cantidadVenta: (producto.stock),
+                  estado: true
+              });
+              //await producto.save();
+          }else{
+              //console.log(`Venta registrada del  ${producto.nombre} - cantidad ${cantidad}`);
+              resolve({
+                  msg: `Venta registrada del  ${producto.nombre} - cantidad ${cantidad}`,
+                  precio: producto.precio,
+                  cantidad,
+                  subtotal: producto.precio * cantidad, 
+                  cantidadStock: (producto.stock - cantidad),
+                  cantidadVenta: cantidad,
+                  estado: true
+              });
+              //await producto.save();
+          }
+      };
+  }else{
+      reject('No se pudo aplicar el descuento del stock del articulo');
+  };
+
+});
+
+const getProducto = async (element, id, cantidad) =>{
+  try {
+       const producto = await Producto.findById(id);
+
+       setTimeout(()=>{
+       getProductoStock(producto, cantidad)
+       .then( (resultado) =>{
+           // console.log(`El resultado es: ${resultado.msg}`);
+           // console.log(`El resultado es: ${resultado.cantidad}`);
+           // console.log(`El resultado es: ${resultado.estado}`);
+           if(resultado.estado){
+               element.estado = resultado;
+               element.cantidad = resultado.cantidadVenta;
+               element.subtotal = (element.cantidad   * element.inf.precio);
+               producto.stock = resultado.cantidadStock;
+
+               console.log("***********************");
+               console.log(resultado);
+
+               return element.subtotal
+               //await producto.save();
+           }else{
+               element.estado = resultado;
+               element.cantidad = 0;
+               element.subtotal = (element.cantidad * element.inf.precio);
+               console.log("***********************");
+               console.log(resultado);
+               producto.stock = resultado.cantidadStock;
+
+           };
+
+       })
+       .catch( (error) =>{
+           console.log(error.message);
+       });
+       }, 3000);  
+
+  } catch (error) {
+       console.log(error.message);
+  }
+
+};
+
 
 
 const prueba = (req, res) => {
